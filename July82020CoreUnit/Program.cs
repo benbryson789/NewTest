15cc3607b243aca64380e2af7bc7ee4030afc4e1
benbryson789 <deborashelby@gmail.com>
2020-07-08 22:01:52 -0400

    first commit

---
+++ July82020CoreUnit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace July82020CoreUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
